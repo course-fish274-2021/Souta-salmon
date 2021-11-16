2cde9db074559b5fe27227f12c4e4a0ecc5f62fc
BillChao123 <bsaechao1@gmail.com>
2021-11-16 10:20:25 -0800

    remove fish with scale length

--- fish-analysis.R
+++ fish-analysis.R
@@ -2,7 +2,11 @@ fish_data <-read.csv("data/Gaeta_etal_CLC_data.csv")
 
 library(dplyr)
 
-#adding column to denote fish is big and small
+#adding column to denote fish is big and small using 300
 
 fish_data_cat <- fish_data %>%
-  mutate(length_cat = ifelse(length > 200, "big", "small"))
+  mutate(length_cat = ifelse(length > 300, "big", "small"))
+
+#exercise 2
+
+
