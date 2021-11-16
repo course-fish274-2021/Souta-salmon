96af2c9c20902b3b53d85d46fbf8edb363cdc542
BillChao123 <bsaechao1@gmail.com>
2021-11-16 10:10:53 -0800

    add data commit

---
+++ fish-analysis.R
@@ -0,0 +1,8 @@
+fish_data <-read.csv("data/Gaeta_etal_CLC_data.csv")
+
+library(dplyr)
+
+#adding column to denote fish is big and small
+
+fish_data_cat <- fish_data %>%
+  mutate(length_cat = ifelse(length > 200, "big", "small"))
